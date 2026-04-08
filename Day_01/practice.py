f57d5366d6850a76ad6dd14d2bbfd041bf2aeb82
Dhayalan <murugesandhayalan609@gmail.com>
2026-04-08 17:28:40 +0530

    Day 01 — Dev setup + Python Hello World

--- Day_01/practice.py
+++ Day_01/practice.py
@@ -2,4 +2,49 @@
 # Topic: Install Python 3.11+, VSCode + Python extension. Run first script: print(), variables, comments, arithmetic operators.
 
 # Write your code below:
+print("Hello Fellow Learners")
+print("this is day 1 in learning python")
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+#Variables
 
+name="Daya"           # stores text
+age="20"              # stores a number
+height=5.9           # stores a decimal
+is_student=True   # stores True or False
+
+print(name)
+print(age)
+print(height)
+print(f"My name is {name} and im {age} years old and my height is {height} ft and im a student")
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Arithmetic operators
+x=10
+y=3
+print("add=",x+y)
+print("Sub=",x-y)
+print("mul=",x*y)
+print("div=",x/y)
+print("floor_div=",x+y) #simlar to divison but no decimal
+print("mod=",x%y)
+print("exponentiation=",x**y)
+
+"""The % (modulo) is very commonly used in interviews to check if a number is even or odd: if num % 2 == 0 means "if there's no remainder when divided by 2 → even"""
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#practice
+#Write a mini calculator 
+price=100
+width=30
+final_price=price-width
+print("final price=",final_price)
+
+#calculate area of a rectangle
+length =10
+width=5
+area=length*width
+print("Area:",area)
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+"""End of Day1 python ...Be consistent one day you will become what you want"""
--- Day_01/practice.sql
+++ Day_01/practice.sql
@@ -2,4 +2,13 @@
 -- Topic: Install MySQL 8 + MySQL Workbench. Create first database: CREATE DATABASE roadmap_db; USE roadmap_db;
 
 -- Write your queries below:
+/*A database is like an Excel file. A table is like a sheet. SQL is the language you use to talk to it — "give me all rows where age is over 18", "insert a new row", "delete old records". Every backend app stores data in a database, and SQL is how you access it.*/
 
+
+
+-- Create a new database
+create database onepiece_db;
+-- Switch to using it
+use onepiece_db;
+-- Verify you are in the right database
+SELECT DATABASE();
